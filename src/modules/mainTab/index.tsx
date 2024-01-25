31474f48b78fdb64d8b193ba37b440b0199d7d68
luobin <luobin@unipus.cn>
2024-01-25 16:38:23 +0800

    fix: update & add image system selection

--- src/modules/mainTab/index.tsx
+++ src/modules/mainTab/index.tsx
@@ -4,12 +4,38 @@ import { Image, Text, View, TouchableOpacity } from "react-native"
 import { styles } from './styles'
 import icon_tab_publish from '@src/assets/icon_tab_publish.png'
 import Shopping from "../shopping"
+import { ImagePickerResponse, launchImageLibrary } from "react-native-image-picker"
 
 const BottomTab = createBottomTabNavigator()
 
 export default () => {
   const TabBar = ({ state, descriptors, navigation }: any) => {
     const { routes, index } = state;
+
+    const onPublishPress = () => {
+      launchImageLibrary(
+        {
+          mediaType: 'photo', // 指令类型
+          quality: 1,
+          includeBase64: true,
+        },
+        // 回调
+        (res: ImagePickerResponse) => {
+          const { assets } = res;
+          console.log('assets', assets);
+
+          if (!assets?.length) {
+            console.log('选择图片失败');
+            return;
+          }
+          const {
+            uri, width, height, fileName, fileSize, type
+          } = assets[0];
+          console.log(`uri=${uri}, width=${width}, height=${height}`);
+          console.log(`fileName=${fileName}, fileSize=${fileSize}, type=${type}`);
+        }
+      );
+    }
 
     return (
       <View style={styles.tabBarContainer}>
@@ -23,7 +49,7 @@ export default () => {
               <TouchableOpacity
                 key={label}
                 style={styles.tabItem}
-              // onPress={onPublishPress}
+                onPress={onPublishPress}
               >
                 <Image style={styles.icon_tab_publish} source={icon_tab_publish} />
               </TouchableOpacity>
